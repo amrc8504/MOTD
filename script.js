f108de93aee8256e354c166ef32a7cf1692c8e02
Batman <anthony.chalabis01@gmail.com>
2022-05-11 23:57:14 -0500

    Fixed URL

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 function copyLink() {
-  navigator.clipboard.writeText("https://amrc8504.github.io/MOTD/")
+  navigator.clipboard.writeText("https://docs.google.com/forms/d/e/1FAIpQLSfg_6fmVVGQiUW4OK6sL6hk_iStiK3LiMB99hUtdOUdD9FkRw/viewform?usp=sf_link")
 }
